68721bac117b0ff87ce7308e3980e64b298aa950
lmingul <2217561701@qq.com>
2021-09-02 00:47:56 +0800

    feat: 添加导航栏配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: lmingul
  * @Date: 2021-09-01 23:33:49
  * @LastEditors: lmingul
- * @LastEditTime: 2021-09-02 00:16:40
+ * @LastEditTime: 2021-09-02 00:44:32
  * @FilePath: \project-test\vuepress-demo\vuepress-demo\docs\.vuepress\config.js
  * @Description: 
  */
@@ -13,9 +13,19 @@
      themeConfig:{
          logo:"/assets/image/logo.jpg",
          nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'External', link: 'https://google.com' },
+            { text: '首页', link: '/' },
+            { 
+                text: '前端', 
+                ariaLabel:"前沿技术",
+                items:[
+                    {text:"Vue",link: '/guide/'},
+                    {text:"React",link: '/guide/'}
+                ]
+                },
+            { text: '指南', link: '/guide/' },
+            { text: '扩展', link: 'https://google.com' },
+            { text: '技术栈', link: '' },
+            { text: '后端', link: '' },
           ]
      }
  }
