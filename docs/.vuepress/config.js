0be13bfec86a6099acbd7160558c1758deed0c40
lmingul <2217561701@qq.com>
2021-09-05 15:47:02 +0800

    feat: 新增导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: lmingul
  * @Date: 2021-09-01 23:33:49
  * @LastEditors: lmingul
- * @LastEditTime: 2021-09-02 00:44:32
+ * @LastEditTime: 2021-09-05 15:40:54
  * @FilePath: \project-test\vuepress-demo\vuepress-demo\docs\.vuepress\config.js
  * @Description: 
  */
@@ -12,20 +12,46 @@
      description:"vuepress demo",
      themeConfig:{
          logo:"/assets/image/logo.jpg",
+         // 导航栏配置
          nav: [
             { text: '首页', link: '/' },
+            { text: '指南', link: '/guide/' },
             { 
-                text: '前端', 
+                text: '前端技术', 
                 ariaLabel:"前沿技术",
                 items:[
                     {text:"Vue",link: '/guide/'},
                     {text:"React",link: '/guide/'}
                 ]
-                },
-            { text: '指南', link: '/guide/' },
-            { text: '扩展', link: 'https://google.com' },
-            { text: '技术栈', link: '' },
-            { text: '后端', link: '' },
-          ]
+            },
+            {
+                text:'后端技术',
+                ariaLabel:"前沿技术",
+                items:[
+                    {text:"JAVA",link: '/guide/'},
+                    {text:"Pathon",link: '/guide/'}
+                ]
+            },
+            {
+                text:"问题记录",
+                link: '/guide/' 
+            },
+            { text: '技术指引', link: '' },
+            {
+                text:"网站扩展",
+                ariaLabel:"xxx",
+                items:[
+                    {text:"vuepress",link:"https://vuepress.vuejs.org/zh/"},
+                    {text:"GitHub",link:"https://github.com/"},
+                    {text:"Vue",link:"https://vuejs.org/"}
+                ],
+                
+            },
+          ],
+         // 侧边栏配置 
+         sidebar:[
+
+         ],
+          
      }
  }
